c1473c0e12844b9e9d47148785ce1d47ed58ac94
Nicolas Portmann <ndportmann@gmail.com>
2020-02-23 17:04:41 +0100

    Enforce kernel version 5.5

--- src/IoUring.Transport/Internals/OsCompatibility.cs
+++ src/IoUring.Transport/Internals/OsCompatibility.cs
@@ -5,16 +5,16 @@ namespace IoUring.Transport.Internals
 {
     internal static class OsCompatibility
     {
-        // Enforce Linux with kernel version >= 5.4
+        // Enforce Linux with kernel version >= 5.5
         public static bool IsCompatible
         {
             get
             {
                 if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return false;
-                
+
                 var osVersion = Environment.OSVersion.Version;
                 if (osVersion.Major > 5) return true;
-                return osVersion.Major == 5 && osVersion.Minor >= 4;
+                return osVersion.Major == 5 && osVersion.Minor >= 5;
             }
         }
     }

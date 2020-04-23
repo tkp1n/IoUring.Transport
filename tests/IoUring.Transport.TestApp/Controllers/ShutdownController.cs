02686faa02eed0128b76c5dea0fab6ffdf1b16c2
Nicolas Portmann <ndportmann@gmail.com>
2020-04-23 06:35:36 +0000

    Add shutdown endpoint to test app

---
+++ tests/IoUring.Transport.TestApp/Controllers/ShutdownController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace IoUring.TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShutdownController : ControllerBase
+    {
+        private readonly IHostApplicationLifetime _lifetime;
+
+        public ShutdownController(IHostApplicationLifetime lifetime)
+        {
+            _lifetime = lifetime;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _lifetime.StopApplication();
+            return "Shutting down";
+        }
+    }
+}

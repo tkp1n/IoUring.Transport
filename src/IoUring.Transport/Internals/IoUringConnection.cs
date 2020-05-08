b059a5a0d3b92443fcc3ba4ef64d30a30915179e
Nicolas Portmann <ndportmann@gmail.com>
2020-05-08 20:14:45 +0200

    Give DuplexPipe its own file

--- src/IoUring.Transport/Internals/IoUringConnection.cs
+++ src/IoUring.Transport/Internals/IoUringConnection.cs
@@ -110,42 +110,5 @@ namespace IoUring.Transport.Internals
         private void RemoveFlag(ConnectionState flag) => Flags &= ~flag;
         private static bool HasFlag(ConnectionState flag, ConnectionState test) => (flag & test) != 0;
         private static ConnectionState SetFlag(ConnectionState flag, ConnectionState newFlag) => flag | newFlag;
-
-        internal sealed class DuplexPipe : IDuplexPipe
-        {
-            public DuplexPipe(PipeReader reader, PipeWriter writer)
-            {
-                Input = reader;
-                Output = writer;
-            }
-
-            public PipeReader Input { get; }
-
-            public PipeWriter Output { get; }
-
-            public static DuplexPipePair CreateConnectionPair(PipeOptions inputOptions, PipeOptions outputOptions)
-            {
-                var input = new Pipe(inputOptions);
-                var output = new Pipe(outputOptions);
-
-                var transportToApplication = new DuplexPipe(output.Reader, input.Writer);
-                var applicationToTransport = new DuplexPipe(input.Reader, output.Writer);
-
-                return new DuplexPipePair(applicationToTransport, transportToApplication);
-            }
-
-            // This class exists to work around issues with value tuple on .NET Framework
-            public readonly struct DuplexPipePair
-            {
-                public IDuplexPipe Transport { get; }
-                public IDuplexPipe Application { get; }
-
-                public DuplexPipePair(IDuplexPipe transport, IDuplexPipe application)
-                {
-                    Transport = transport;
-                    Application = application;
-                }
-            }
-        }
     }
 }
